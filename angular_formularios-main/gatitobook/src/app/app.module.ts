800ca997fe8bc360752bdfe55057baf2b3392b26
devHikary <wrkkarina@gmail.com>
2021-06-23 19:51:12 -0300

    curso angular formulario - Aula 2

--- angular_formularios-main/gatitobook/src/app/app.module.ts
+++ angular_formularios-main/gatitobook/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +12,7 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
