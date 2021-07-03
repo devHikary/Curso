ffaa5a9d6fb8aa482206dae9bdfe02b9c45bd324
devHikary <wrkkarina@gmail.com>
2021-07-02 22:33:05 -0300

    Controle_Fluxo Aula 2

--- angular_formularios-main/gatitobook/src/app/animais/animal/animal.component.ts
+++ angular_formularios-main/gatitobook/src/app/animais/animal/animal.component.ts
@@ -1,34 +1,31 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 const API = environment.apiURL;
 
 @Component({
   selector: 'app-animal',
   templateUrl: './animal.component.html',
-  styleUrls: ['./animal.component.css']
+  styleUrls: ['./animal.component.css'],
 })
 export class AnimalComponent implements OnInit {
-
   private urlOriginal = '';
 
   @Input() descricao = '';
 
-  @Input() set url(url:string) {
-    if(url.startsWith('data')){
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
       this.urlOriginal = url;
-    } else{
-      this.urlOriginal = `${API}/img/${url}`;
+    } else {
+      this.urlOriginal = `${API}/imgs/${url}`;
     }
   }
 
-  get url() {
+  get url(): string {
     return this.urlOriginal;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
