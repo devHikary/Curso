564d0c5cda8ca3f5bc7cf3baed79e8d61de17d87
devHikary <wrkkarina@gmail.com>
2021-07-07 21:11:08 -0300

    Controle_fluxo Aula 3.3

--- angular_formularios-main/gatitobook/src/app/animais/animais-routing.module.ts
+++ angular_formularios-main/gatitobook/src/app/animais/animais-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetalhesAnimalComponent } from './detalhes-animal/detalhes-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 
 const routes: Routes = [
   {
     path: '',
     component:ListaAnimaisComponent,
-  }
+  },
+  {
+    path: ':animalId',
+    component: DetalhesAnimalComponent,
+  },
 ];
 
 @NgModule({
